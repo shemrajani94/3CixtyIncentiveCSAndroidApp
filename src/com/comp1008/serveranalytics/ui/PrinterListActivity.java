1dfdf3c205f4c8c1fc1558dd038474704baaf0e7
Alex Emms <me@alexemms.com>
2013-03-15 10:08:49 +0000

    Added LabListActivity with sublist of computers. Edits to MainMenuActivity

--- src/com/comp1008/serveranalytics/ui/PrinterListActivity.java
+++ src/com/comp1008/serveranalytics/ui/PrinterListActivity.java
@@ -1,5 +1,24 @@
 package com.comp1008.serveranalytics.ui;
 
-public class PrinterListActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.comp1008.serveranalytics.R;
+
+public class PrinterListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_printer_list);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+	// Inflate the menu; this adds items to the action bar if it is present.
+	getMenuInflater().inflate(R.menu.printer_list, menu);
+	return true;
+    }
 
 }
