2cabf8b82a8c3671672d8943537104f60406731c
Saagar <saagar.hemrajani@gmail.com>
2013-03-17 20:19:19 +0000

    created printer list, some work done on settings (doesnt work yet)

--- src/com/comp1008/serveranalytics/ui/PrinterListActivity.java
+++ src/com/comp1008/serveranalytics/ui/PrinterListActivity.java
@@ -15,9 +15,12 @@ import android.widget.ListView;
 
 import com.comp1008.serveranalytics.R;
 
+/* This class should show a list of printers, which are clickable and on click
+ * will load a page with the printer's details. getPrinters() function should be added. */
+
 public class PrinterListActivity extends Activity implements AdapterView.OnItemClickListener{
 
-	private String printers[] = {"Printer1", "Printer2", "Printer3"};
+	private String printers[] = {"Printer1", "Printer2", "Printer3", "Printer4","Printer5", "Printer6", "Printer7", "Printer8"};
 	
 	
     @Override
@@ -25,14 +28,20 @@ public class PrinterListActivity extends Activity implements AdapterView.OnItemC
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.activity_printer_list);
 	ArrayAdapter<String> adapter = new ArrayAdapter<String>(PrinterListActivity.this, R.layout.lab_list_custom, printers);
+	// Creates an ArrayAdapter for the String array printers.
 	ListView listOfPrinters = (ListView) findViewById(R.id.PrintersList);
+	// This ListView is in the XML activity_printer_list 
 	listOfPrinters.setAdapter(adapter);
+	listOfPrinters.setOnItemClickListener(this);
     }
     
+    
+    /* This Method should get list of printers in lab on button click, 
+	 * Passes string of printer name corresponding to button pressed to its respective page   */
+    // For now it just passes the string to the main menu
     public void onItemClick(AdapterView adapter, View v, int position, long id){
     	String PrinterName = printers[position];
-    	Log.v("labClick", "Clicked lab " + PrinterName);
-    	Intent intent = new Intent(PrinterListActivity.this, SettingsActivity.class);
+    	Intent intent = new Intent(PrinterListActivity.this, MainMenuActivity.class);
     	Bundle printer = new Bundle();
     	printer.putString("printer", PrinterName);
     	intent.putExtras(printer);
--- src/com/comp1008/serveranalytics/ui/SettingsActivity.java
+++ src/com/comp1008/serveranalytics/ui/SettingsActivity.java
@@ -1,5 +1,75 @@
 package com.comp1008.serveranalytics.ui;
 
-public class SettingsActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
+import com.comp1008.serveranalytics.R;
+
+
+public class SettingsActivity extends Activity {
+	//IGNORE THIS NONE OF IT WORKS
+/*
+	 Intent intent;
+	 Button doneSettingsButton;	
+	 
+		RadioGroup radFreq;
+		RadioButton radOption1;
+		RadioButton radOption2;
+		RadioButton radOption3;
+		RadioButton radOption4;
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_settings);
+		runit();
+	}
+	
+	
+	 public void runit() {
+			// Creates our views, in this case 3 buttons
+		 
+			
+			radFreq = (RadioGroup) findViewById(R.id.radio_group);
+			radOption1 = (RadioButton)findViewById(R.id.radio0);
+			radOption2 = (RadioButton)findViewById(R.id.radio1);
+			radOption3 = (RadioButton)findViewById(R.id.radio2);
+			radOption4 = (RadioButton)findViewById(R.id.radio3);
+			
+			
+			doneSettingsButton = (Button) findViewById(R.id.doneSettingsButton);
+			doneSettingsButton.setOnClickListener((OnClickListener) this); 
+		    }
+
+	 public void onClick(View v) {
+			
+			switch(v.getId()){
+			
+			case(R.id.doneSettingsButton):
+			    intent = new Intent(SettingsActivity.this, MainMenuActivity.class);
+			    SettingsActivity.this.startActivity(intent);
+			break;
+			
+			default:
+			    break;
+			}
+		    }
+		
+	
+	 @Override
+	    public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.settings_page, menu);
+		return true;
+	    }
+
+	*/
 }
+
